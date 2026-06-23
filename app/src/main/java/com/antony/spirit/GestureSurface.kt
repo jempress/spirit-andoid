bff493ddc3bd264192c2b5920f81e1dbdb5aeb49
jempress <antoniajempress@gmal.com>
2026-06-24 02:16:48 +0300

    fixed awaitFirstDown import

--- app/src/main/java/com/antony/spirit/GestureSurface.kt
+++ app/src/main/java/com/antony/spirit/GestureSurface.kt
@@ -9,7 +9,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.PointerEventPass
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.awaitFirstDown
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.ui.input.pointer.changedToUp
 import androidx.compose.ui.unit.dp
 import kotlin.math.abs
